d1c161b1844404317aa9ce45e5015fd3296af9ed
Kaushik Yelne <42184951+kaushikyelne@users.noreply.github.com>
2020-10-23 11:50:15 +0530

    Update views.py

--- news/newsweb/views.py
+++ news/newsweb/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-
-    news_api_request = requests.get("http://newsapi.org/v2/everything?q=apple&from=2020-10-20&to=2020-10-20&sortBy=popularity&apiKey=3f0c4d518cf840189a1a2ae5568e3284")
+3
+    news_api_request = requests.get("http://newsapi.org/v2/everything?q=apple&from=2020-10-20&to=2020-10-20&sortBy=popularity&apiKey=f0c4d518cf840189a1a2ae5568e3284")
     
     api = json.loads(news_api_request.content)
     
